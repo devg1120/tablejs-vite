faf2c654b831dbc8cf54f92a09e0e20a8a4ed7ea
devg1120 <devg1120@gmail.com>
2025-05-03 15:47:40 +0900

    up

---
+++ v3-tablejs-vite/plugins/editor.js
@@ -0,0 +1,14 @@
+import EditorJS from '@editorjs/editorjs'
+
+export default ({ $axios }, inject) => {
+  inject('editor', {
+    EditorJS: ({ holder, placeholder, data }) => {
+      return new EditorJS({
+        holder,
+        placeholder,
+        data,
+      })
+    },
+  })
+}
+
